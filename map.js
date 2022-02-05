0d84a341c11d7b0b970d04ed508918e722236411
sacortesro <sacortesro@unal.edu.co>
2022-02-04 19:07:18 -0500

    Map web page

---
+++ map.js
@@ -0,0 +1,72 @@
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el primer punto.
+function Punto1() {
+    document.getElementById("Info").innerHTML = '<h2>Casa Bojack Horseman</h2>' +
+        '<div class="row px-5">' +
+        '<span class="col-sm-4">' +
+        '<img src="images/map/bojack_horseman_house.png" class="img-fluid" alt="Casa Bojack Horseman">' +
+        '</span>' +
+        '<p class="col-sm-8">' +
+        'Úbicada en la 1475 Luck Hoof Avenue, es la residencia del protagonista Bojack. La casa fué comprada por Bojack en la década de los 80\'s, gracias al éxito que tuvo con su personaje protagónico de Horsin\' Around. Al inicio de la serie la casa también es habitada por Todd Chavez, que por años había estado durmiendo en el sofá, esto hasta que Bojack lo echa. Bojack vive en el lugar hasta el final de la temporada 2, después de esto, la serie toma lugar en otras locaciones, volviendo a la casa en ocasiones esporádicas. En medio de la temporada 6 Bojack se ve obligado a vender la casa debido a problemas económicos y la demanda de la compañia Xerox.' +
+        '</p>'
+    document.getElementById("Info").scrollIntoView();
+}
+
+function Punto2() {
+    document.getElementById("Info").innerHTML = '<h2>Prision Hollywood Hills</h2>' +
+        '<div class="row px-5">' +
+        '<span class="col-sm-4">' +
+        '<img src="images/map/Prision_hollywoodHills.jpg" class="img-fluid" alt="Prision Hollywood Hills">' +
+        '</span>' +
+        '<div class="col-sm-8">' +
+        '<p>De la prisión se muestran las celdas y la cafetería, la cual tiene la D tachada debido al robo del letrero de Hollywoo.</p>' +
+        '<p>En esta prisión es dónde retienen a Todd Chavez en el capítulo seis de la temporada 1. Es enviado allí después de que Princess Carolyn utiliazara su negocio de turismo para lavar dinero. Al final escapa del lugar gracias a que el helicóptero que carga la D se estrellara con la pared de la prisión.</p>' +
+        '<p>La prisión vuelve a aparecer en la 6 temporada, para el capitulo final, después de que Bojack sea sentenciado a 14 meses depués de irrumpir en su antigua casa, ahora propiedad de una nueva familia. En la prisión Bojack decide dirigir una producción de teatro, por lo que le permiten irse durante un fin de semana a la Boda de Princess Carolyn y Judah.</p>' +
+        '</div>';
+    document.getElementById("Info").scrollIntoView();
+}
+
+function Punto3() {
+    document.getElementById("Info").innerHTML = '<h2>Adept Agencia de adopción</h2>' +
+        '<div class="row px-5">' +
+        '<span class="col-sm-4">' +
+        '<img src="images/map/Adept_Adoption_Agency.png" class="img-fluid" alt="Adept Adoption Agency">' +
+        '</span>' +
+        '<div class="col-sm-8">' +
+        '<p>Es una locación recurrente en la 5 temporada. Hace parte del arco de personaje de Pricess Carolyn, que despúes de una charla motivacional decide pagar $ 60.000 USD para adoptar un niño. La trabajadora social que atiende a Princess es una persona persoza y grosera llamada Tracy. </p>' +
+        '<p> Pricess Carolyn a lo largo de la temporada visita el lugar varias veces en búsqueda de oficializar la adopción del hijo de Sadie, personaje que conoce gracias a la casa de adopción. Al finalizar la temporada, después de que Sadie reconsiderara quedarse o no con la bebe, decide darle la bebé a Princess Carolyn. </p>' +
+        '</div>';
+    document.getElementById("Info").scrollIntoView();
+}
+function Punto4() {
+    document.getElementById("Info").innerHTML = '<h2>Agencia VIM</h2>' +
+        '<div class="row px-5">' +
+        '<span class="col-sm-4">' +
+        '<img src="images/map/agency_vim.jpg" class="img-fluid" alt="Agencia VIM">' +
+        '</span>' +
+        '<div class="col-sm-8">' +
+        '<p> VIM es una compañia fundada por Princess Carolyn, inicialmente era un proyecto con Rutabaga Rabitowitz, en la 2 temporada, pero todo se vino abajo cuando Princess se enterará que Rutabaga estaba intentando repara su relación con su exesposa, cuando ellos dos habían estado en una relación por casi 3 meses. Finalmente Princess Carolyn decide fundar la agencia en la temporada 3. Alli contrata a su asistente Judah Mannowdog y a Diane como coordinadora de redes sociales. </p> <p> Después de su fundación en la agencia VIM toma lugar una variedad de sucesos importantes en la vida de Princess Carolyn, esto debido a que ella pasa la mayor cantidad de tiempo allí, es esclava de su trabajo. </p> <p>En la 6 temporada Judah le decide regresar a la compañia a apoyar a Princess Carolyn, después de que lo despidiera en el final de la 3 temporada. Allí, es dónde después de que Princess Carolyn le aconsejara a Judah no dar toda su vida al trabajo. Depués de esto Princess Carolyn decide ir al concierto de su banda, pero cuando llega allí uno de los miembros anuncia que van a tener que tocar las versiones instrumentales de sus canciones, dado que su vocalista no se había presentado.</p> <p> Al regresar a la oficina Princess Carolyn descubre a Judah trabajando, quien le dice que prefiere estar con ella. Entonces después de intercambiar unas palabras, Princess Carolyn le pide a Judah que toque una de sus canciones, en medio de lo cual él le dice que la ama. </p>'+
+        '</div>';
+    document.getElementById("Info").scrollIntoView();
+}
+function Punto5() {
+    document.getElementById("Info").innerHTML = '<h2>Tabbywood apartments</h2>' +
+        '<div class="row px-5">' +
+        '<span class="col-sm-4">' +
+        '<img src="images/map/tabbywood_apartment.png" class="img-fluid" alt="Tabbywood apartments">' +
+        '</span>' +
+        '<p class="col-sm-8">' +
+            'Es el lugar en la serie dónde Princess Carolyn vive. Residencia que Princess comparte con varios de sus novios y con Todd Chavez a lo largo de varios episodios de la 4 temporada.'+
+        '</p>';
+    document.getElementById("Info").scrollIntoView();
+}
+function Punto6() {
+    document.getElementById("Info").innerHTML = '<h2>Bellicans</h2>' +
+        '<div class="row px-5">' +
+        '<span class="col-sm-4">' +
+        '<img src="images/map/Bellcians.jpg" class="img-fluid" alt="Bellicans">' +
+        '</span>' +
+        '<div class="col-sm-8">' +
+            '<p> Es una locación recurrente en la serie y el bar favorito de Bojack. En este lugar se presenta por primera vez al personaje de Bojack. Durante la 2 temporada, después del éxito del libro de Bojack, se observa un cartel que dice "Bojack drinks here".</p> <p> A través de la serie se puede observar a Bojack y otros personajes ir a tomar varias veces al lugar. En la 6 temporada se observa a Bojack tomando con el Doctor Champ, su doctor de rehabilitación, allí tienen una convesación introspectiva que termina con el Doctor Champ reacayendo en el alcoholismo y Bojack sintiendo la culpa de haberlo conducido nuevamente por ese camino.</p>'+
+        '</div>';
+    document.getElementById("Info").scrollIntoView();
+}
